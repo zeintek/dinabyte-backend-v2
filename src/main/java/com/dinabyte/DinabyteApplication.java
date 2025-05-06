054eb6ebb080615ddbb3fc638f9e6301007eb8d1
zeintek <luis.alvarez.mart@gmail.com>
2025-05-05 22:01:29 -0600

    inicial

---
+++ src/main/java/com/dinabyte/DinabyteApplication.java
@@ -0,0 +1,13 @@
+package com.dinabyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinabyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinabyteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinabyte/DinabyteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinabyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinabyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
